b06654ca65b473029b5a7181b52143f0d4580e09
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-05-13 19:11:31 +0800

    1) def add() 2) fix test_initialize() 3) def test_add()

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 # function to initialize 2048
@@ -6,3 +7,18 @@ def initialize():
     # create a 4x4 matrix containing zeros
     mat = np.zeros(shape=(4, 4), dtype='int')
     return mat
+
+
+# function to add number at any random empty cell
+def add(mat):
+    # choose random index for row and column
+    r = random.randint(0, 3)
+    c = random.randint(0, 3)
+    # while loop breaks only if the cell is empty; otherwise chooses another pair of index
+    while mat[r][c] != 0:
+        r = random.randint(0, 3)
+        c = random.randint(0, 3)
+    # place either 2 or 4 at that empty random cell
+    mat[r][c] = random.choice([2, 4])
+    return mat
+
--- test_logic.py
+++ test_logic.py
@@ -1,12 +1,31 @@
 import unittest
 import logic
+import numpy as np
 
 
 class MyTestCase(unittest.TestCase):
     def test_initialize(self):
-        self.assertEqual(
-            logic.initialize().all(),
-            logic.np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]).all())
+        # test_initialize-01
+        actual = logic.initialize()
+        expected = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+        self.assertEqual(np.testing.assert_array_equal(actual, expected), None)
+
+    def test_add(self):
+        # test_add-01
+        actualMat = logic.add(np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]))
+        actual = np.count_nonzero(actualMat == 2) + np.count_nonzero(actualMat == 4)
+        expected = 1
+        self.assertEqual(actual, expected)
+        # test_add-02
+        actualMat = logic.add(np.array([[2, 32, 0, 64], [8, 4, 4, 0], [1024, 0, 32, 2], [0, 4, 16, 4]]))
+        actual = np.count_nonzero(actualMat == 2) + np.count_nonzero(actualMat == 4)
+        expected = 7
+        self.assertEqual(actual, expected)
+        # test_add-03
+        actualMat = logic.add(np.array([[2, 2, 2, 2], [4, 4, 0, 4], [2, 2, 2, 2], [4, 4, 4, 4]]))
+        actual = np.count_nonzero(actualMat == 2) + np.count_nonzero(actualMat == 4)
+        expected = 16
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
